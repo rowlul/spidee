2de5bd0f7120315bfe90d25b276d260491b71833
finitefish <wauderl11@gmail.com>
2022-01-22 16:38:03 +0200

    Return error as soon as it's not nil

--- cli/util/files.go
+++ cli/util/files.go
@@ -8,14 +8,14 @@ import (
 )
 
 func BuildFilesFromContext(c *cli.Context) ([]sendpart.File, error) {
-	var err error
-
 	var files []sendpart.File
 	for _, f := range c.StringSlice("file") {
-		var file *os.File
-		file, err = os.OpenFile(f, os.O_RDONLY, os.ModeAppend)
+		file, err := os.OpenFile(f, os.O_RDONLY, os.ModeAppend)
+		if err != nil {
+			return files, err
+		}
 		files = append(files, sendpart.File{Name: file.Name(), Reader: file})
 	}
 
-	return files, err
+	return files, nil
 }
