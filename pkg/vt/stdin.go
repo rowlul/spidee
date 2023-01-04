4b9f773428b690d03c9eaa95075be8616bd53547
rowlul <96617295+rowlul@users.noreply.github.com>
2023-01-04 21:13:24 +0200

    Handle stdin in vt package

---
+++ pkg/vt/stdin.go
@@ -0,0 +1,22 @@
+package vt
+
+import (
+	"bufio"
+	"os"
+)
+
+func IsStdin() bool {
+	stat, _ := os.Stdin.Stat()
+	return (stat.Mode() & os.ModeCharDevice) == 0
+}
+
+func ReadStdin() []string {
+	var lines []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
