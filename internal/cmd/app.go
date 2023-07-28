a3e615180dd9690d511ac129181b7bfe68a41de7
rowlul <96617295+rowlul@users.noreply.github.com>
2023-07-28 17:20:18 +0300

    Set version if not set via ldflags

--- internal/cmd/app.go
+++ internal/cmd/app.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/diamondburned/arikawa/v3/api/webhook"
 	"github.com/diamondburned/arikawa/v3/discord"
@@ -13,8 +14,9 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-// Version specified via ldflags
-var Version string
+// Version specified via ldflags. Defaults to v?-yyyyMMddHHmmss if not set via
+// ldflags.
+var Version string = "v?-" + time.Now().Format("20060102150405")
 
 func NewApp() *cli.App {
 	cli.VersionPrinter = func(ctx *cli.Context) {
